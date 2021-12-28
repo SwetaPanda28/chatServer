e556027d86fc274f03019e6079a6c7da1c836197
SwetaPanda28 <19cse296.swetaranipanda@giet.edu>
2021-12-28 21:17:58 +0530

    login complete signal created ready for login testing and registration

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,10 +1,6 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
-from django.db.models.fields import related
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -12,8 +8,13 @@ class UserModel(models.Model):
     privateKey=models.CharField(max_length=100,blank=True)
     publicKey=models.CharField(max_length=100,blank=True)
     masterpassword=models.CharField(max_length=100,blank=True)
-    image_field = models.ImageField(null=True)
-    phoneNumber = PhoneNumberField(unique = True, null = True)
+    image_field = models.ImageField(null=True,blank=True)
+    phoneNumber = PhoneNumberField(unique = True, null = True,blank=True)
+    authUser = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    detailsFilled=models.BooleanField(default=False,blank=True)
+
+    def __str__(self):
+        return f"{self.authUser.email} : {self.authUser.username}"
 
 class Msg(models.Model):
     msg=models.CharField(max_length=10000)
---
+++ mainapp/signals.py
@@ -0,0 +1,9 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from . import models
+
+@receiver(post_save,sender=User)
+def createUserModel(instance,created,**kwargs):
+    if(created):
+        models.UserModel.objects.create(authUser=instance)
